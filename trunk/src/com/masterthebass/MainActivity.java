e1696f7ee616a82def5ee4ccbd9a2d12dcab89f9
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2012-12-09 20:22:36 +0000

    Cleaned up warnings
    Removed unused imports

--- trunk/src/com/masterthebass/MainActivity.java
+++ trunk/src/com/masterthebass/MainActivity.java
@@ -1,13 +1,10 @@
 package com.masterthebass;
 
 import android.os.Bundle;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 
 public class MainActivity extends Activity {
 	
--- trunk/src/com/masterthebass/SettingsActivity.java
+++ trunk/src/com/masterthebass/SettingsActivity.java
@@ -1,11 +1,8 @@
 package com.masterthebass;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 
 public class SettingsActivity extends Activity{
