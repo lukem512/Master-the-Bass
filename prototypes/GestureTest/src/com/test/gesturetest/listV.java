e03f7a588dc735fb121c2761b05026c2fd7cb6ed
Petro Tiurin <pt1620@my.bristol.ac.uk>
2013-01-25 17:45:44 +0200

    Added gestures in prototypes

---
+++ prototypes/GestureTest/src/com/test/gesturetest/listV.java
@@ -0,0 +1,45 @@
+package com.test.gesturetest;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class listV extends ListActivity{
+
+	public static final String TAG = "com.test.gesturetest";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setListAdapter( new ArrayAdapter<String>(this, R.layout.list_1st_item,gesture));
+		ListView list = getListView();
+		list.setTextFilterEnabled(true);
+		list.setOnItemClickListener(new OnItemClickListener()
+			{
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+					// TODO Auto-generated method stub
+					Toast.makeText(getApplicationContext(), ((TextView) arg1).getText(), Toast.LENGTH_SHORT).show();
+					Intent intent = getIntent();
+					String tag = intent.getStringExtra("tag");
+					
+					Log.e(TAG,"the button pressed is " + (Integer.parseInt(tag)-1));					
+					com.test.gesturetest.MainActivity.addTogestureArray(((TextView) arg1).getText(), Integer.parseInt(tag) - 1);
+					finish();
+				}
+			}
+		);				
+	}	
+	static final String[] gesture = new String[]{
+		"Tap","Swipe Down","Swipe Up","Swipe Left","Swipe Right","Hold","Tap","Swipe Down","Swipe Up","Swipe Left","Swipe Right","Hold"};		
+}
