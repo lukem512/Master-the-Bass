bf85f1d2cd6853d86ca1018cea7addc8f269686d
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-01-28 16:03:18 +0000

    Added stub FilterManager
    This should enable work to be done on the Filter menu.

---
+++ trunk/src/com/masterthebass/FilterManager.java
@@ -0,0 +1,25 @@
+package com.masterthebass;
+
+public class FilterManager {
+	// TODO: implement
+	int[] getFiltersList () {
+		return null;	
+	}
+	
+	// TODO: implement
+	String getFilterName (int ID) {
+		return "";
+	}
+	
+	// TODO: implement
+	void toggleFilter (int ID) {
+	}
+	
+	// TODO: implement
+	void enableFilter (int ID) {
+	}
+	
+	// TODO: implement
+	void disableFilter (int ID) {
+	}
+}
