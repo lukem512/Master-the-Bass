49cdc6837987d0b0c761c41e45549fac0ff4401e
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-01-29 13:45:37 +0000

    Updated Prototype to use actual sound gen code

--- prototypes/GeneratedSoundTest/src/masterthebass/prototypes/generatedsoundtest/SoundManager.java
+++ prototypes/GeneratedSoundTest/src/masterthebass/prototypes/generatedsoundtest/SoundManager.java
@@ -3,7 +3,7 @@ package masterthebass.prototypes.generatedsoundtest;
 import android.util.Log;
 
 public class SoundManager{
-	private int period;
+	private double Final;
 	
 	
 	/* Members */
@@ -22,45 +22,54 @@ public class SoundManager{
 	// Returns a byte array of the sound in 16-bit WAV PCM format
 	public byte[] generateTone(double duration, double frequency, double volume, int sampleRate) {
 		int numSamples = (int) Math.ceil(sampleRate * duration);
-		double sample[] = new double[numSamples];
-		byte generatedSnd[] = new byte[2 * numSamples];
-		int i, idx;
-		
-		double sampleByFreq = (sampleRate/frequency);
-		double twopi = (2*Math.PI);
-		int volValue = (int) (32767*volume);
-		
+		byte generatedSnd[];
+
 		// Sanity check volume
 		if (volume < 0.0) {
 			volume = 0.0;
 		} else if (volume > 1.0) {
 			volume = 1.0;
 		}
-		
-		Log.d(logTag+".generateTone", "Starting with period of " + period + ".");
 
-        // Generate the sine wave
+		generatedSnd = doGenerateTone(numSamples, frequency, volume, sampleRate, Final);
+        
+        // Save starting offset for next tone
+        Final = (numSamples + Final) % (sampleRate/frequency);
+        
+        return generatedSnd;
+    }
+	
+	private byte[] doGenerateTone (int numSamples, double frequency, double volume, int sampleRate, double offset) {
+		double sample = 0.0;
+		byte generatedSnd[] = new byte[2 * numSamples];
+		int i, idx;
+
+		double sampleByFreq = (sampleRate/frequency);
+		double twopi = (2*Math.PI);
+		int volValue = (int) (32767*volume);
+
+        // Generate the tone
 		idx = 0;
-        for (i = 0; i < numSamples; i++) {	
+        for (i = 0; i < numSamples; i++) {      	
         	// Sine value
-            sample[i] = Math.sin(twopi * ((i+period)/sampleByFreq));
+            sample = Math.sin(twopi * ((i + offset)/sampleByFreq));
             
             // Scale to max amplitude
-            sample[i] = sample[i] * volValue;
+            sample = sample * volValue;
             
-            // Generate 16-bit sample
-            final short val = (short) (sample[i]);
+            // Generate 16-bit samples
+            final short val = (short) sample;
             generatedSnd[idx++] = (byte) (val & 0x00ff);
             generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
         }
         
-        period += i;
-        
+        // Return the sound
         return generatedSnd;
-    }
+	}
+	
 
 	// Resets the period of the waveform
 	public void resetPeriod() {
-		period = 0;
+		Final = 0;
 	}
 }
