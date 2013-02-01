6df4c23890bd56e57d018b7f46310c58d445cec4
Jon E Rankin <jr0436@bris.ac.uk>
2013-02-01 12:30:36 +0000

    added customisable range to Noise Filter

--- prototypes/GeneratedSoundTest/src/masterthebass/prototypes/generatedsoundtest/NoiseFilter.java
+++ prototypes/GeneratedSoundTest/src/masterthebass/prototypes/generatedsoundtest/NoiseFilter.java
@@ -3,21 +3,29 @@ import java.util.Random;
 
 
 public class NoiseFilter extends Filter  {
-
+	int range;
+	
 	public NoiseFilter(int ID, String name) {
 		super(ID, name);
 		// TODO Auto-generated constructor stub
+		
+		//set default range
+		range = 20;
 	}
 	
 
 
+		void setRange(int newrange){
+			this.range = newrange;
+		}
+		
 	@Override
 	byte[] applyFilter (byte[] rawPCM){
 	    Random generator = new Random();
 		int count = rawPCM.length;
 		 for (int i=0;i<count;i++)
 		 {
-			 int randomIndex = (generator.nextInt(20) - 10);
+			 int randomIndex = (generator.nextInt(range) - (range/2));
 		     rawPCM[i] =(byte)(rawPCM[i] + randomIndex);
 		   }
 
