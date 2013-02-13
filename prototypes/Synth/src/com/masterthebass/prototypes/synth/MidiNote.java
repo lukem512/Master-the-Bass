42547fa27074f62c59176158b2f0809d3f956370
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-02-13 15:47:55 +0000

    Completed note names
    And partial completion of MIDI note numbers

--- prototypes/Synth/src/com/masterthebass/prototypes/synth/MidiNote.java
+++ prototypes/Synth/src/com/masterthebass/prototypes/synth/MidiNote.java
@@ -2,6 +2,8 @@ package com.masterthebass.prototypes.synth;
 
 public final class MidiNote {
 	
+	/* Note names */
+	
 	public final static float A0 = 27.5f;
 	public final static float B0 = 30.868f;
 	
@@ -13,7 +15,21 @@ public final class MidiNote {
 	public final static float A1 = 55f;
 	public final static float B1 = 61.735f;
 	
-	// TODO - C2 to B3
+	public final static float C2 = 65.406f;
+	public final static float D2 = 73.416f;
+	public final static float E2 = 82.407f;
+	public final static float F2 = 87.307f;
+	public final static float G2 = 97.999f;
+	public final static float A2 = 110f;
+	public final static float B2 = 123.47f;
+	
+	public final static float C3 = 130.81f;
+	public final static float D3 = 146.83f;
+	public final static float E3 = 164.81f;
+	public final static float F3 = 174.61f;
+	public final static float G3 = 196f;
+	public final static float A3 = 220f;
+	public final static float B3 = 246.94f;
 	
 	public final static float C4 = 261.63f;
 	public final static float D4 = 293.67f;
@@ -23,11 +39,33 @@ public final class MidiNote {
 	public final static float A4 = 440f;
 	public final static float B4 = 493.88f;
 	
-	// TODO - C5 to B7
+	public final static float C5 = 523.25f;
+	public final static float D5 = 587.33f;
+	public final static float E5 = 659.26f;
+	public final static float F5 = 698.46f;
+	public final static float G5 = 783.99f;
+	public final static float A5 = 880f;
+	public final static float B5 = 987.77f;
+	
+	public final static float C6 = 1046.5f;
+	public final static float D6 = 1174.7f;
+	public final static float E6 = 1318.5f;
+	public final static float F6 = 1396.9f;
+	public final static float G6 = 1568f;
+	public final static float A6 = 1760f;
+	public final static float B6 = 1975.5f;
+	
+	public final static float C7 = 2093f;
+	public final static float D7 = 2349.3f;
+	public final static float E7 = 2637f;
+	public final static float F7 = 2793f;
+	public final static float G7 = 3136f;
+	public final static float A7 = 3520f;
+	public final static float B7 = 3951.1f;
 	
 	public final static float C8 = 4186f;
 	
-	// TODO MIDI numbers
+	/* MIDI numbers */
 	
 	public final static float MIDI21 = A0;
 	public final static float MIDI22 = 29.135f;
@@ -35,8 +73,88 @@ public final class MidiNote {
 	public final static float MIDI24 = C1;
 	public final static float MIDI25 = 34.648f;
 	public final static float MIDI26 = D1;
+	public final static float MIDI27 = 38.891f;	
+	public final static float MIDI28 = E1;
+	public final static float MIDI29 = F1;
 	
-	// TODO MIDI27 to MIDI107
+	public final static float MIDI30 = B7;
+	public final static float MIDI31 = B7;
+	public final static float MIDI32 = B7;
+	public final static float MIDI33 = B7;
+	public final static float MIDI34 = B7;
+	public final static float MIDI35 = B7;
+	public final static float MIDI36 = B7;
+	public final static float MIDI37 = B7;
+	public final static float MIDI38 = B7;
+	public final static float MIDI39 = B7;
+	public final static float MIDI40 = B7;
+	public final static float MIDI41 = B7;
+	public final static float MIDI42 = B7;
+	public final static float MIDI43 = B7;
+	public final static float MIDI44 = B7;
+	public final static float MIDI45 = B7;
+	public final static float MIDI46 = B7;
+	public final static float MIDI47 = B7;
+	public final static float MIDI48 = B7;
+	public final static float MIDI49 = B7;
+	public final static float MIDI50 = B7;
+	public final static float MIDI51 = B7;
+	public final static float MIDI52 = B7;
+	public final static float MIDI53 = B7;
+	public final static float MIDI54 = B7;
+	public final static float MIDI55 = B7;
+	public final static float MIDI56 = B7;
+	public final static float MIDI57 = B7;
+	public final static float MIDI58 = B7;
+	public final static float MIDI59 = B7;
+	public final static float MIDI60 = B7;
+	public final static float MIDI61 = B7;
+	public final static float MIDI62 = B7;
+	public final static float MIDI63 = B7;
+	public final static float MIDI64 = B7;
+	public final static float MIDI65 = B7;
+	public final static float MIDI66 = B7;
+	public final static float MIDI67 = B7;
+	public final static float MIDI68 = B7;
+	public final static float MIDI69 = B7;
+	public final static float MIDI70 = B7;
+	public final static float MIDI71 = B7;
+	public final static float MIDI72 = B7;
+	public final static float MIDI73 = B7;
+	public final static float MIDI74 = B7;
+	public final static float MIDI75 = B7;
+	public final static float MIDI76 = B7;
+	public final static float MIDI77 = B7;
+	public final static float MIDI78 = B7;
+	public final static float MIDI79 = B7;	
+	public final static float MIDI80 = B7;
+	public final static float MIDI81 = B7;
+	public final static float MIDI82 = B7;
+	public final static float MIDI83 = B7;
+	public final static float MIDI84 = B7;
+	public final static float MIDI85 = B7;
+	public final static float MIDI86 = B7;
+	public final static float MIDI87 = B7;
+	public final static float MIDI88 = B7;
+	public final static float MIDI89 = B7;
+	public final static float MIDI90 = B7;
+	public final static float MIDI91 = B7;
+	public final static float MIDI92 = B7;
 	
+	public final static float MIDI93 = A6;
+	public final static float MIDI94 = 1864.7f;
+	public final static float MIDI95 = B6;
+	public final static float MIDI96 = C7;
+	public final static float MIDI97 = 2217.5f;
+	public final static float MIDI98 = D7;
+	public final static float MIDI99 = 2489f;
+	public final static float MIDI100 = E7;
+	public final static float MIDI101 = F7;
+	public final static float MIDI102 = 2960f;
+	public final static float MIDI103 = G7;
+	public final static float MIDI104 = 3322.4f;
+	public final static float MIDI105 = A7;
+	public final static float MIDI106 = 3729.3f;
+	public final static float MIDI107 = B7;
 	public final static float MIDI108 = C8;
 }
