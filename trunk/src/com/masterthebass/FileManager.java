2286bde49539660639a494a422cbbc26ea9dc4a4
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-02-18 11:26:26 +0000

    Added writing to FileManager

--- trunk/src/com/masterthebass/FileManager.java
+++ trunk/src/com/masterthebass/FileManager.java
@@ -2,12 +2,17 @@ package com.masterthebass;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import android.os.Environment;
+import android.util.Log;
 
 public class FileManager {
+		private String logTag = "FileManager";
+	
 		/* Constructor */
 	
 		public FileManager() {
@@ -41,6 +46,84 @@ public class FileManager {
 		    
 		    return result;
 		}
+		
+		/* Write binary file function */
+		
+		public boolean writeBinaryFile(String path, String filename, byte[] data) {
+			return writeBinaryFile(path, filename, data, 0);
+		}
+		
+		public boolean writeBinaryFile(String path, String filename, byte[] data, int offset) {
+			File fp;
+			FileOutputStream fs;
+			
+			try {
+				fp = new File(path, filename);
+			} catch (NullPointerException e) {
+				Log.w(logTag+".writeBinaryFile", "Null filename supplied.");
+				e.printStackTrace();
+				return false;
+			} 
+			
+			try {
+				fs = new FileOutputStream(new File(path, filename));
+			} catch (FileNotFoundException e) {
+				Log.w(logTag+".writeBinaryFile", "Could not open file "+path+"/"+filename);
+				e.printStackTrace();
+				return false;
+			}
+			
+			try {
+				Log.d(logTag+".writeBinaryFile", "Attempting to write data of length " + data.length + " bytes to file at offset " + offset);
+				fs.write(data, offset, data.length);
+				fs.close();
+			} catch (IOException e) {
+				Log.w(logTag+".writeBinaryFile", "Could not write to file " + fp.getAbsolutePath());
+				e.printStackTrace();
+				return false;
+			}
+			
+			Log.d(logTag+".writeBinaryFile", "Wrote " + data.length + " bytes to " + fp.getAbsolutePath());
+			
+			return true;
+		}
+		
+		public boolean appendBinaryFile(String path, String filename, byte[] data) {			
+			File fp;
+			FileOutputStream fs;
+			
+			try {
+				fp = new File(path, filename);
+			} catch (NullPointerException e) {
+				Log.w(logTag+".appendBinaryFile", "Null filename supplied.");
+				e.printStackTrace();
+				return false;
+			} 
+			
+			try {
+				fs = new FileOutputStream(new File(path, filename), true);
+			} catch (FileNotFoundException e) {
+				Log.w(logTag+".appendBinaryFile", "Could not open file "+path+"/"+filename);
+				e.printStackTrace();
+				return false;
+			}
+			
+			try {
+				Log.d(logTag+".appendBinaryFile", "Attempting to append data of length " + data.length);
+				fs.write(data, 0, data.length);
+				fs.close();
+			} catch (IOException e) {
+				Log.w(logTag+".appendBinaryFile", "Could not write to file " + fp.getAbsolutePath());
+				e.printStackTrace();
+				return false;
+			}
+			
+			Log.d(logTag+".appendBinaryFile", "Wrote " + data.length + " bytes to " + fp.getAbsolutePath());
+			
+			return true;
+		}
+		
+		/* Static functions */
 		
 		public static String getSDPath() {
 			return Environment.getExternalStorageDirectory().getPath();
