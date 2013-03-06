14555e54f285df70e94e287e410a639b692ef1fd
Luke Mitchell <Luke.Mitchell.2011@my.bristol.ac.uk>
2013-03-06 21:46:15 +0000

    Spacing.

--- trunk/src/com/masterthebass/FilterManager.java
+++ trunk/src/com/masterthebass/FilterManager.java
@@ -10,8 +10,6 @@ import java.util.Set;
 
 import android.util.Log;
 
-
-
 public class FilterManager implements Serializable {
 	public final static String TAG = "com.masterthebass.FILTERMANAGER";
 	private static final long serialVersionUID = 754321647758395857L;
