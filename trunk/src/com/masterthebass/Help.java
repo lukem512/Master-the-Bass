7b0c362e0d6df77ebf0a656b8efb39ad3f63b7ad
Petro Tiurin <pt1620@my.bristol.ac.uk>
2013-03-18 16:14:55 +0000

    Filled help manu with text.

--- trunk/src/com/masterthebass/Help.java
+++ trunk/src/com/masterthebass/Help.java
@@ -1,8 +1,18 @@
 package com.masterthebass;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Display;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class Help extends Activity {
 
@@ -10,6 +20,32 @@ public class Help extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_help);
+		setupText();
+	}
+	//load text from file and format textview
+	private void setupText(){
+		//formatting textview
+		TextView t = (TextView)findViewById(R.id.texthelp);
+		t.setMovementMethod(new ScrollingMovementMethod());
+		t.setTextSize(20);
+		WindowManager mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+	    Display mDisplay = mWindowManager.getDefaultDisplay();
+	    t.setMaxLines(mDisplay.getHeight()/20);
+		
+		//reading text
+		InputStream is = getResources().openRawResource(R.raw.help);
+		StringBuilder total = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+		try {
+			while ((line = br.readLine()) != null) {
+			    total.append(line);
+			    total.append('\n');
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		t.setText(total);
 	}
 
 	@Override
